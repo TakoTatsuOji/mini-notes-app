ccbe9be08676bd8e4b4a934a9bd2722e53324c41
Haemoffy <fuwansuu@gmail.com>
2024-07-02 17:10:35 +0800

    renamed $title to $website_title

--- controllers/index.php
+++ controllers/index.php
@@ -1,11 +1,11 @@
 <?php
 
-$title = "Notes";
+$website_title = "Notes";
 
 $db_config = require 'dbconfig.php';
 
 $db = new Database($db_config['dsn_params'], $db_config['username'], $db_config['password']);
 
-$notes = $db->querySelect(["id", "title"]);
+$notes = $db->querySelectAll(["id", "title"]);
 
 require 'views/index.view.php';
