adcae33cdb3283331cc644a92ab986295eaf219a
Haemoffy <fuwansuu@gmail.com>
2024-07-04 18:00:50 +0800

    finishing up form and added partial for input err

--- views/edit.view.php
+++ views/edit.view.php
@@ -2,19 +2,20 @@
 
 <div class="notes-container">
     <?php require 'partials/goback.php' ?>
-    <form action="" method="post">
+    <form action="/editnote" method="post">
         <div>
             <label for="title">Title</label>
-            <input type="text" name="title" id="title" placeholder="Title of the note" value="">
+            <input type="text" name="title" id="title" placeholder="Title of the note" value="<?= $current_note_content['title'] ?>">
         </div>
         <div>
             <label for="body">Body</label>
-            <textarea name="body" id="body" cols="30" rows="10" placeholder="Body of the note"></textarea>
+            <textarea name="body" id="body" cols="30" rows="10" placeholder="Body of the note"><?= $current_note_content['body'] ?></textarea>
         </div>
         <div>
             <button type="submit">Edit Note</button>
         </div>
     </form>
+    <?php require 'partials/error.php' ?>
 </div>
 
 <?php require 'partials/foot.php' ?>
