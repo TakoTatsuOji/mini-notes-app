f093616de6839e8d8aad83b4ca987e82601d7a78
Haemoffy <fuwansuu@gmail.com>
2024-07-04 17:57:43 +0800

    build logic for edit view with current note values

--- controllers/edit.php
+++ controllers/edit.php
@@ -1,5 +1,14 @@
 <?php
 
-$website_title = "Edit Note";
+
+$db_config = require 'dbconfig.php';
+
+$db = new Database($db_config['dsn_params'], $db_config['username'], $db_config['password']);
+
+$id = $_GET['id'];
+
+$current_note_content = $db->querySelectOne([':id' => $id], ['title', 'body']);
+
+$website_title = "Edit {$current_note_content['title']} Note";
 
 require 'views/edit.view.php';
