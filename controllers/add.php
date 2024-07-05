0fe8a22773828a3dd542c107ecbe4036f5b967b5
Haemoffy <fuwansuu@gmail.com>
2024-07-05 20:09:19 +0800

    merged POSTing logic from extra controllers

--- controllers/add.php
+++ controllers/add.php
@@ -2,4 +2,26 @@
 
 $website_title = "Add Note";
 
+$db_config = require 'dbconfig.php';
+
+$db = new Database($db_config['dsn_params'], $db_config['username'], $db_config['password']);
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $errors = [];
+
+    $title = isInputBlank($_POST['title']);
+    $body = isInputBlank($_POST['body']);
+
+    if ($title == null || $body == null) {
+        $errors['empty'] = "Don't leave the fields blank";
+    } else {
+        $db->queryInsert([':title' => $title, ':body' => $body]);
+    
+        header("Location: /");
+    
+        die();
+    }
+
+}
+
 require 'views/add.view.php';
--- controllers/edit.php
+++ controllers/edit.php
@@ -1,6 +1,5 @@
 <?php
 
-
 $db_config = require 'dbconfig.php';
 
 $db = new Database($db_config['dsn_params'], $db_config['username'], $db_config['password']);
@@ -10,5 +9,23 @@ $id = $_GET['id'];
 $current_note_content = $db->querySelectOne([':id' => $id], ['title', 'body']);
 
 $website_title = "Edit {$current_note_content['title']} Note";
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $errors = [];
+
+    $title = isInputBlank($_POST['title']);
+    $body = isInputBlank($_POST['body']);
+
+    if ($title == null || $body == null) {
+        $errors['empty'] = "Don't leave the fields blank";
+    } else {
+        $db->queryUpdate([':id' => $id, ':title' => $title, ':body' => $body]);
+    
+        header("Location: /");
+    
+        die();
+    }
+
+}
 
 require 'views/edit.view.php';
